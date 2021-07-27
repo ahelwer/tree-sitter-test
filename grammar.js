d2d79918937d3930621944d3ccd4ca2b72ed3f39
Andrew Helwer <ahelwer@protonmail.com>
2021-07-27 11:14:27 -0400

    Added partially-ordered static precedence

--- grammar.js
+++ grammar.js
@@ -6,15 +6,21 @@ module.exports = grammar({
     [$.proof_step, $.lt],
   ],
 
+  precedences: $ => [
+    ['13-13', '10-10', '5-5']
+  ],
+
   rules: {
     source_file: $ => repeat($.unit),
     unit: $ => choice($.proof, $._expr),
     proof: $ => prec.right(repeat1($.proof_step)),
     proof_step: $ => seq($.proof_token, $._expr),
     _expr: $ => choice($.identifier, $.number, $._infix_op),
-    _infix_op: $ => choice($.lt, $.gt),
-    lt: $ => prec.left(seq($._expr, '<', $._expr)),
-    gt: $ => prec.left(seq($._expr, '>', $._expr)),
+    _infix_op: $ => choice($.lt, $.gt, $.add, $.mul),
+    lt: $ => prec.left('5-5', seq($._expr, '<', $._expr)),
+    gt: $ => prec.left('5-5', seq($._expr, '>', $._expr)),
+    add: $ => prec.left('10-10', seq($._expr, '+', $._expr)),
+    mul: $ => prec.left('13-13', seq($._expr, '*', $._expr)),
 
     identifier: $ => /[a-zA-Z]+/,
     number: $ => /\d+/,
