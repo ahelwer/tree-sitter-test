c2d463215aa59f3475aed3f9987afc26543fa570
Andrew Helwer <ahelwer@protonmail.com>
2021-07-27 10:11:04 -0400

    Basic example

--- grammar.js
+++ grammar.js
@@ -11,16 +11,23 @@ module.exports = grammar({
   ],
 
   rules: {
-    source_file: $ => choice($.map, $._expr),
-
-    in: $ => '\\in',
-    gt: $ => '>',
-
+    source_file: $ => repeat(choice($.map, $.proof, $._expr)),
     map: $ => seq($.quantification, $.arrow, $._expr),
     quantification: $ => seq($.identifier, $.in, $.identifier),
-    identifier: $ => /[a-zA-Z]+/,
-    _expr: $ => choice($.identifier, $.infix_op),
+    proof: $ => prec.right(repeat1(seq($.proof_token, $._expr))),
+    _expr: $ => choice($.identifier, $.number, $.infix_op),
     infix_op: $ => prec.left(1, seq($._expr, $.infix_op_symbol, $._expr)),
-    infix_op_symbol: $ => choice($.turnstile, $.in, $.gt),
+    infix_op_symbol: $ => choice($.turnstile, $.in, $.lt, $.gt),
+
+    identifier: $ => /[a-zA-Z]+/,
+    number: $ => /\d+/,
+    in: $ => '\\in',
+    lt: $ => '<',
+    gt: $ => '>',
+    proof_token: $ => seq(
+      token(prec(1, '<')),
+      token.immediate(/\d+/),
+      token.immediate('>')
+    ),
   }
 });
